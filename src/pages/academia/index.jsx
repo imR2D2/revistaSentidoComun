0a63d2812047b4e049d0e493e1c0bc1f0b25f06c
Arturo Hernández <102141714+imR2D2@users.noreply.github.com>
2025-02-02 17:32:54 -0600

    upd: agregar pageSize=10

--- src/pages/academia/index.jsx
+++ src/pages/academia/index.jsx
@@ -41,7 +41,7 @@ const Blog = () => {
   const [loading, setLoading] = useState(true);
 
   const [page, setPage] = useState(1);
-  const pageSize = 1
+  const pageSize = 5
   const [total, setTotal] = useState(0)
 
   useEffect(() => {
